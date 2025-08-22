b0970cfeb7814731b22c73c1293945147085e726
LaisZagati <laisviana97@gmail.com>
2025-08-22 13:53:16 -0400

    Vanilla Weather Search / API Integration

--- src/script.js
+++ src/script.js
@@ -1,11 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city"); // Target the city element from html using ID}
 
+  cityElement.innerHTML = response.data.city; // Update h1 city name to API result
+  temperatureElement.innerHTML = Math.round(temperature); // Update HTML with current city with the current temperature
+}
 
+function searchCity(city) {
+  //Make an API call to get the weather data for the city
+  let apiKey = "f093ocaff400a6043tff45112437b840";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault(); // Prevent the default form submission behavior
   let searchInput = document.querySelector("#search-form-input"); // Get the user input
-  let cityElement = document.querySelector("#city"); // Target the city element from html using ID}
-  cityElement.innerHTML = searchInput.value; // Update h1 city name to user input value
+
+  searchCity(searchInput.value); // Call the function to search for the city
 }
 
 let searchFormElement = document.querySelector("#search-form"); // Target form element from html using ID
