3deb3da283796a3846925dbd05921f8cc346eaae
LaisZagati <laisviana97@gmail.com>
2025-08-22 16:48:45 -0400

    Injected more weather data

--- src/script.js
+++ src/script.js
@@ -2,9 +2,38 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city"); // Target the city element from html using ID}
+  let descriptionElement = document.querySelector("#description"); // Target the description element from html using ID
+  let humidityElement = document.querySelector("#humidity"); // Target the humidity element from html using ID
+  let windElement = document.querySelector("#wind-speed"); // Target the wind element from html using ID
+  let timeElement = document.querySelector("#time"); // Target the time element from html using ID
+  let date = new Date(response.data.time * 1000); // Convert the time from seconds to milliseconds
 
   cityElement.innerHTML = response.data.city; // Update h1 city name to API result
+  descriptionElement.innerHTML = response.data.condition.description; // Update description to API result
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`; // Update description to API result
+  windElement.innerHTML = `${response.data.wind.speed} km/h`; // Update description to API result
+  timeElement.innerHTML = formatDate(date); // Update HTML with current city with the current time calling formatDate function
   temperatureElement.innerHTML = Math.round(temperature); // Update HTML with current city with the current temperature
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -23,6 +52,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form"); // Target form element from html using ID
 searchFormElement.addEventListener("submit", handleSearchSubmit); // Add event listener to the form when submit is clicked
-
 
 searchCity("Paris"); // Initial call to load weather data for Paris
